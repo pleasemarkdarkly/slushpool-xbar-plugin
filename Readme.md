c624bcbb66a75f67347b4b843b31e5a022084e0d
Mark Phillips <me@compuglobalhypermegacorp.com>
2021-03-29 06:12:39 -0700

    sample function calls

--- Readme.md
+++ Readme.md
@@ -1,6 +1,22 @@
 # Slushpool Xbar Plugin
 Xbar (formerly known as Bitbar) Plugin to monitor Slushpool mining account. The sample API key is read-only and likely may not work so be sure to create your own from your Slushpool account. This Plugin take advantage of the [Slushpool APIs](https://help.slushpool.com/en/support/solutions/articles/77000433512-api-configuration-guide) to provide information about Pool, User Profile, or Worker Stats. Instruction on how to generate your Access Key is provided in the aforementioned guide.
 
+### MenuBar Graphic
+ ![Mining Graphic](./images/placeholder.jpg)
+
+ ```base64
+ ABC
+ ```
+
+* username
+* hash_rate_5m (hash_rate_unit)
+* hash_rate_60m (hash_rate_unit)
+* hash_rate_24h (hash_rate_unit)
+* confirmed_reward (bitcoin_logo)
+* ok_workers
+* off_workers
+* low_workers
+
 ### Example API Key
 The plugin requires an environment variable VAR_SLUSHPOOL_API_KEY, the example read-only key `Zm1oK8hGaDExOBaE` is provided as an example, however as previously mentioned likely has been disabled.
 
@@ -16,10 +32,34 @@ The plugin requires an environment variable VAR_SLUSHPOOL_API_KEY, the example r
 ### Example
 `https://slushpool.com/accounts/workers/json/btc/`
 
-```
+```bash
 curl https://slushpool.com/accounts/profile/json/btc/ -H "SlushPool-Auth-Token: Zm1oK8hGaDExOBaE"
 curl https://slushpool.com/accounts/workers/json/btc/ -H "SlushPool-Auth-Token: Zm1oK8hGaDExOBaE"
 
 ```
+### User Profile Output
+
+```json
+{
+    "username": "markep",
+    "btc": {
+        "confirmed_reward": "0.00144336",
+        "unconfirmed_reward": "0.00019718",
+        "estimated_reward": "0.00004134",
+        "send_threshold": "0.00500000",
+        "hash_rate_unit": "Gh/s",
+        "hash_rate_5m": 28203.7617,
+        "hash_rate_60m": 28974.6332,
+        "hash_rate_24h": 29377.1755,
+        "hash_rate_scoring": 29272.1846,
+        "hash_rate_yesterday": 29633.6409,
+        "low_workers": 0,
+        "off_workers": 1,
+        "ok_workers": 3,
+        "dis_workers": 1
+    }
+}
+```
+
 
 
--- slushpool.5m.js
+++ slushpool.5m.js
@@ -17,31 +17,108 @@ const bitbar = require('bitbar');
 # < xbar.var > string(VAR_SLUSHPOOL_API_KEY=""): API key to get access to remote data from slushpool.</xbar.var >
 # < xbar.var > boolean(VAR_VERBOSE = true): Whether to be verbose or not.</xbar.var >
 # < xbar.var > list(VAR_STYLE = "normal"): Which style to use. [small, normal, big]</xbar.var >
+
+Use curl rate.sx/0.75BTC
 */
 
-USER_ACCOUNT_URL = 'https://slushpool.com/accounts/profile/json/btc/ -H "SlushPool-Auth-Token: ';
+(async () => {
 
-bitbar([
-	{
-		text: '🚀',
-		color: bitbar.darkMode ? 'white' : 'red',
-		dropdown: true
-	},
-	bitbar.separator,
-	{
-		text: 'Unicorns',
-		color: '#ff79d7',
-		submenu: [
-			{
-				text: ':tv: Video',
-				href: 'https://www.youtube.com/watch?v=9auOCbH5Ns4'
-			},
-			{
-				text: ':book: Wiki',
-				href: 'https://en.wikipedia.org/wiki/Unicorn'
-			}
-		]
-	},
-	bitbar.separator,
-	'Ponies'
-]);
+	const child_process = require('child_process');
+	const https = require('https');
+
+	const BITCOIN_ICON = 'iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAAACXBIWXMAABYlAAAWJQFJUiTwAAABY0lEQVRIx2P4z0AdyEBzg1DAdIYfQJgCZHmCWdsYMAFRBs0BC2UAWT5g1p6hbZAggwIcrgALVQNZSWDWAQY24g3qwRtJ/xgeMqxkCGJgotQgGLzAoEUdg/4zvGQQIxzYLAyODF/gQv0MlgwWDK4MOQxbgV5DKG0nLtZ2wIUykII2EMmoU8QZtAWrQQwMB+HiDygzaDNc/CQlBskwfIKLN5JrkAxDFsMTuOh9BiFSDXoHDI2HDB9RlJ1kECc2r20hkI5OMXhQxyAQzCTNoDJgaAgAvaLLEMkwn+EbkuLvDBLkR78yUoD/Z0gn3yAGhnwk5V2UGBRGLYNmICkvIGzQLqwG8TA0oJQAVvgMymcoYehg+AUXWgoM0kygWC/DbpQ4+89wjYERt0FiRNeNX4GlFJ505EykMacZDPGn7HwCBnxiOMcwjcGJcOEvzqADh2vBQk1AVhaYdZCBc7TKpqJBA9ZiAwDMH49EXcmY2QAAAABJRU5ErkJggg=='
+	const VAR_SLUSHPOOL_API_KEY = 'Zm1oK8hGaDExOBaE'; // process.env.VAR_SLUSHPOOL_API_KEY;
+
+	const CONVERT_BTC_TO_USD = 'rate.sx/'
+	const BTC_BALANCE = 0.75;
+
+	let slushpool_api_token = VAR_SLUSHPOOL_API_KEY;
+	let btc_to_usd_balance = '';
+
+	let boldColor = 'white';
+	try {
+		child_process.execSync('defaults read -g AppleInterfaceStyle', { stdio: 'ignore' });
+	} catch (err) {
+		// AppleInterfaceStyle not set, which means user has light menu bar style
+		boldColor = 'black';
+	}
+
+	// Font, Color, and Emoji Settings
+	const redText = '| color=red size=14';
+	const normalText = '| size=14';
+	const boldText = `| color=${boldColor} size=14`;
+	const heartEmoji = '♥︎';
+	const brokenHeartEmoji = `♡`;
+
+	// Prototype without env first...
+	// Makes sure user overrided default configs
+	if (VAR_SLUSHPOOL_API_KEY === '<YOUR_SLUSHPOOL_API_KEY_HERE>') {
+		console.log(brokenHeartEmoji, 'Slushpool user API_KEY not set!', brokenHeartEmoji);
+		process.exit();
+	}
+
+	let options = {
+		hostname: 'rate.sx',
+		port: 443,
+		path: '/' + BTC_BALANCE + 'BTC',
+		method: 'GET'
+	}
+
+	let req = await https.request(options, res => {
+		res.on('data', d => {
+			console.log(d.toString());
+			btc_to_usd_balance = d.toString();
+		});
+	});
+	req.on('error', error => { console.error(error); });
+	req.end();
+
+	const headers = { 'SlushPool-Auth-Token': VAR_SLUSHPOOL_API_KEY };
+	options = {
+		hostname: 'slushpool.com',
+		port: 443,
+		path: '/accounts/workers/json/btc/',
+		method: 'GET',
+		headers: headers
+	};
+
+	let slushpool_response = '';
+
+	req = await https.request(options, res => {
+		res.on('data', d => {
+			console.log(d.toString());
+			slushpool_response = d.toString();
+		});
+	})
+
+	req.on('error', error => { console.error(error); });
+	req.end();
+
+
+
+
+	bitbar([
+		{
+			text: 'Total BTC' + btc_to_usd_balance,
+			color: bitbar.darkMode ? 'white' : 'red',
+			dropdown: true
+		},
+
+		bitbar.separator,
+		{
+			text: 'Unicorns',
+			color: '#ff79d7',
+			submenu: [
+				{
+					text: ':tv: Video',
+					href: 'https://www.youtube.com/watch?v=9auOCbH5Ns4'
+				},
+				{
+					text: ':book: Wiki',
+					href: 'https://en.wikipedia.org/wiki/Unicorn'
+				}
+			]
+		},
+	]);
+
+})();
