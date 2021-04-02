b74414ee95c27fff835da5ee11a98dcb4ec6b6a2
Mark Phillips <me@compuglobalhypermegacorp.com>
2021-04-02 16:11:49 -0700

    organized api response data and conversion to dollars

--- slushpool.5m.js
+++ slushpool.5m.js
@@ -18,7 +18,9 @@
 # < xbar.var > boolean(VAR_VERBOSE = true): Whether to be verbose or not.</xbar.var >
 # < xbar.var > list(VAR_STYLE = "normal"): Which style to use. [small, normal, big]</xbar.var >
 
-To convert an image to use with this plugin, use the command `openssl base64 -A -in` with the file and replace with the constant below.
+* I understand the view key is provided, this is to show the plugin works and the permissions for this key example are read-only.
+* Removed the dependancy to bitbar node module to avoid node related issues.
+
 */
 
 const https = require('https');
@@ -27,15 +29,17 @@ const { once } = require('events');
 
 const SLUSHPOOL_MINING_ICON = `⛏️`;
 const BITCOIN_ICON = 'iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAAACXBIWXMAABYlAAAWJQFJUiTwAAABY0lEQVRIx2P4z0AdyEBzg1DAdIYfQJgCZHmCWdsYMAFRBs0BC2UAWT5g1p6hbZAggwIcrgALVQNZSWDWAQY24g3qwRtJ/xgeMqxkCGJgotQgGLzAoEUdg/4zvGQQIxzYLAyODF/gQv0MlgwWDK4MOQxbgV5DKG0nLtZ2wIUykII2EMmoU8QZtAWrQQwMB+HiDygzaDNc/CQlBskwfIKLN5JrkAxDFsMTuOh9BiFSDXoHDI2HDB9RlJ1kECc2r20hkI5OMXhQxyAQzCTNoDJgaAgAvaLLEMkwn+EbkuLvDBLkR78yUoD/Z0gn3yAGhnwk5V2UGBRGLYNmICkvIGzQLqwG8TA0oJQAVvgMymcoYehg+AUXWgoM0kygWC/DbpQ4+89wjYERt0FiRNeNX4GlFJ505EykMacZDPGn7HwCBnxiOMcwjcGJcOEvzqADh2vBQk1AVhaYdZCBc7TKpqJBA9ZiAwDMH49EXcmY2QAAAABJRU5ErkJggg=='
-const VAR_SLUSHPOOL_API_KEY = 'Zm1oK8hGaDExOBaE'; // process.env.VAR_SLUSHPOOL_API_KEY;
+
+// due to the error prone nature of the above path, dependancies, etc, edit script directly vs. .env
+const VAR_SLUSHPOOL_API_KEY = 'Zm1oK8hGaDExOBaE';
 let slushpool_api_token = VAR_SLUSHPOOL_API_KEY;
 
-let BTC_BALANCE = 0.75; // Previous or additional balance to track
-let BTC_REWARD_BALANCE = 0;
-let BTC_TO_USD = 0;
+let BTC_EXISTING_BALANCE = 0.78711908;
+let USD_EXISTING_BTC_BALANCE = 0;
 
-let hash_rates = [];
-let workers = [];
+const numberWithCommas = (x) => {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 
 const getBTCToUSD = async (amount) => {
 	let result;
@@ -131,14 +135,21 @@ const getSlushpoolStats = async (apikey) => {
 	return result
 };
 
+/*
+	Compute ongoing mining balances/estimates, existing balances.
+*/
 const collectSlushpoolInfo = async () => {
 	try {
 		const profile = await getSlushpoolProfile(slushpool_api_token);
 		const stats = await getSlushpoolStats(slushpool_api_token);
 		const { confirmed_reward, unconfirmed_reward, estimated_reward } = profile;
-		const amount = (parseFloat(confirmed_reward) + parseFloat(unconfirmed_reward) + parseFloat(BTC_BALANCE));
-		BTC_TO_USD = await getBTCToUSD(amount);
-		return { profile, stats, total_btc: amount, total_btc_in_usd: BTC_TO_USD };
+		let reward = {};
+		reward.usd_confirmed_reward = await getBTCToUSD(parseFloat(confirmed_reward));
+		reward.usd_unconfirmed_reward = await getBTCToUSD(parseFloat(unconfirmed_reward));
+		reward.usd_estimated_reward = await getBTCToUSD(parseFloat(estimated_reward));
+		// include any permuation of confirmed, unconfirmed, estimate, existing BTC or USD here
+		USD_EXISTING_BTC_BALANCE = await getBTCToUSD(parseFloat(BTC_EXISTING_BALANCE));
+		return { profile, stats, reward, total_btc_in_usd: USD_EXISTING_BTC_BALANCE };
 	} catch (error) {
 		return error;
 	}
@@ -158,9 +169,45 @@ const debugSlushpoolOutputAndFormat = async (bitbar) => {
 	});
 }
 
+const SLUSHPOOL_URL = 'href=https://slushpool.com/dashboard/?c=btc';
+const REFRESH_FALSE = 'refresh=false';
+const SEPARATOR = '---';
+
 const printBitbar = async (bitbar) => {
-	console.log(`⛏️`);
-}
+	const { profile, stats, reward, total_btc_in_usd } = bitbar;
+	const { confirmed_reward, unconfirmed_reward, estimated_reward, hash_rate_unit, hash_rate_5m, hash_rate_60m, hash_rate_24h, ok_workers } = profile;
+	const { worker_keys, workers } = stats;
+
+	// Show in the menu if a worker is off 
+	worker_keys.forEach(k => {
+		if (workers[k].state == 'off') {
+			const worker_name = k.split('.')[1];
+			console.log(`${worker_name.toLowerCase()}`);
+		};
+	});
+	console.log(`${SLUSHPOOL_MINING_ICON} ${numberWithCommas(parseFloat(hash_rate_5m).toFixed(2))} ${hash_rate_unit} (5m)`);
+	console.log(`${SLUSHPOOL_MINING_ICON} ${numberWithCommas(parseFloat(hash_rate_60m).toFixed(2))} ${hash_rate_unit} (60m)`);
+	console.log(`${SLUSHPOOL_MINING_ICON} ${numberWithCommas(parseFloat(hash_rate_24h).toFixed(2))} ${hash_rate_unit} (24h)`)
+	console.log(SEPARATOR);
+	console.log(`${confirmed_reward}/\$${parseFloat(reward.usd_confirmed_reward).toFixed(2)} (confirmed reward) | templateImage=${BITCOIN_ICON}`);
+	console.log(`${unconfirmed_reward}/\$${parseFloat(reward.usd_unconfirmed_reward).toFixed(2)} (unconfirmed reward) | templateImage=${BITCOIN_ICON}`);
+	console.log(`${estimated_reward}/\$${parseFloat(reward.usd_estimated_reward).toFixed(2)} (estimated reward) | templateImage=${BITCOIN_ICON}`);
+	console.log(SEPARATOR);
+	console.log(`OK workers (${ok_workers}) | ${SLUSHPOOL_URL}`)
+	worker_keys.forEach(k => {
+		if (workers[k].state == 'OK') {
+			const worker_name = k.split('.')[1];
+			console.log(SEPARATOR);
+			console.log(`${worker_name.toLowerCase()}`);
+			console.log(`${numberWithCommas(parseFloat(workers[k].last_share))} (last share)`)
+			console.log(`${numberWithCommas(parseFloat(workers[k].hash_rate_5m))} ${workers[k].hash_rate_unit} (5m)`);
+			console.log(`${numberWithCommas(parseFloat(workers[k].hash_rate_60m))} ${workers[k].hash_rate_unit} (60m)`);
+			console.log(`${numberWithCommas(parseFloat(workers[k].hash_rate_24h))} ${workers[k].hash_rate_unit} (24h)`);
+		}
+	});
+	console.log(SEPARATOR); // existing balance 
+	console.log(`${BTC_EXISTING_BALANCE} \$${numberWithCommas(parseFloat(USD_EXISTING_BTC_BALANCE).toFixed(2))} | ${REFRESH_FALSE} templateImage=${BITCOIN_ICON}`);
+};
 
 (async () => {
 
