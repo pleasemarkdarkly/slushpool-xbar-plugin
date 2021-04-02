113e76c08872cbf311b472b0556eb6fa4d9ff8d9
Mark Phillips <me@compuglobalhypermegacorp.com>
2021-04-02 15:01:48 -0700

    updated scripts

--- Readme.md
+++ Readme.md
@@ -1,21 +1,19 @@
-# Slushpool Xbar Plugin
+# Slushpool [Xbar](https://github.com/matryer/xbar) Plugin
 Xbar (formerly known as Bitbar) Plugin to monitor Slushpool mining account. The sample API key is read-only and likely may not work so be sure to create your own from your Slushpool account. This Plugin take advantage of the [Slushpool APIs](https://help.slushpool.com/en/support/solutions/articles/77000433512-api-configuration-guide) to provide information about Pool, User Profile, or Worker Stats. Instruction on how to generate your Access Key is provided in the aforementioned guide.
 
-### MenuBar Graphic
- ![Mining Graphic](./images/placeholder.jpg)
-
- ```base64
- ABC
- ```
-
-* username
-* hash_rate_5m (hash_rate_unit)
-* hash_rate_60m (hash_rate_unit)
-* hash_rate_24h (hash_rate_unit)
-* confirmed_reward (bitcoin_logo)
-* ok_workers
-* off_workers
-* low_workers
+### ⛏️ Rough idea of output
+  * Pending mining payments: (confirmed_reward + unconfirmed_reward)
+  * Balance in USD: (Pending payments + any additional balance)
+  * -separator-
+  * hash_rate (5m): n (hash_rate_unit)
+  * hash_rate (60m): n (hash_rate_unit)
+  * hash_rate (24/h): n (hash_rate_unit)
+  * -separator-
+  * ok-workers
+  * -separator-
+  * dis-workers
+  * -separator-
+  * off-workers
 
 ### Example API Key
 The plugin requires an environment variable VAR_SLUSHPOOL_API_KEY, the example read-only key `Zm1oK8hGaDExOBaE` is provided as an example, however as previously mentioned likely has been disabled.
@@ -61,5 +59,56 @@ curl https://slushpool.com/accounts/workers/json/btc/ -H "SlushPool-Auth-Token: 
 }
 ```
 
+### Worker Output
+
+```json
+{
+  'markep.[auto]': {
+    state: 'dis',
+    last_share: 1616202341,
+    hash_rate_unit: 'Gh/s',
+    hash_rate_scoring: 0,
+    hash_rate_5m: 0,
+    hash_rate_60m: 0,
+    hash_rate_24h: 0
+  },
+  'markep.antminder_t9': {
+    state: 'OK',
+    last_share: 1617184681,
+    hash_rate_unit: 'Gh/s',
+    hash_rate_scoring: 10500.4893,
+    hash_rate_5m: 9640.3261,
+    hash_rate_60m: 11132.968,
+    hash_rate_24h: 5981.7406
+  },
+  'markep.antminer_s9i': {
+    state: 'OK',
+    last_share: 1617184678,
+    hash_rate_unit: 'Gh/s',
+    hash_rate_scoring: 12393.3512,
+    hash_rate_5m: 11332.3428,
+    hash_rate_60m: 13131.9758,
+    hash_rate_24h: 13979.5378
+  },
+  'markep.antminer_s9i_1': {
+    state: 'OK',
+    last_share: 1617184679,
+    hash_rate_unit: 'Gh/s',
+    hash_rate_scoring: 4733.7485,
+    hash_rate_5m: 5645.2621,
+    hash_rate_60m: 4392.7959,
+    hash_rate_24h: 4776.8809
+  },
+  'markep.nvidia-1080': {
+    state: 'off',
+    last_share: null,
+    hash_rate_unit: 'Gh/s',
+    hash_rate_scoring: 0,
+    hash_rate_5m: 0,
+    hash_rate_60m: 0,
+    hash_rate_24h: 0
+  }
+}
+```
 
 
--- slushpool-api.js
+++ slushpool-api.js
@@ -1,41 +1,16 @@
 #!/usr/bin/env /usr/local/bin/node
 
-/*
-# Slushpool Monitoring Xbar Plugin
-#
-# < xbar.title > Slushpool User Account Monitor Plugin </xbar.title >
-# < xbar.version > v1.0</xbar.version >
-# < xbar.author >Mark Phillips</xbar.author >
-# < xbar.author.github >pleasemarkdarkly </xbar.author.github >
-# < xbar.desc > Displays Slushpool Account/Worker Stats </xbar.desc >
-# < xbar.image > https://github.com/pleasemarkdarkly/slushpool-xbar-plugin/images/plugin.jpg </xbar.image>
-# < xbar.dependencies > bash </xbar.dependencies >
-# < xbar.abouturl > https://github.com/pleasemarkdarkly/slushpool-xbar-plugin/Readme.md </xbar.abouturl>
-
-# Variables become preferences in the app:
-#
-# < xbar.var > string(VAR_SLUSHPOOL_API_KEY=""): API key to get access to remote data from slushpool.</xbar.var >
-# < xbar.var > boolean(VAR_VERBOSE = true): Whether to be verbose or not.</xbar.var >
-# < xbar.var > list(VAR_STYLE = "normal"): Which style to use. [small, normal, big]</xbar.var >
-
-To convert an image to use with this plugin, use the command `openssl base64 -A -in` with the file and replace with the constant below.
-*/
-
 const https = require('https');
 const child_process = require('child_process');
 const { once } = require('events');
 
-const SLUSHPOOL_MINING_ICON = `⛏️`;
 const BITCOIN_ICON = 'iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAQAAABLCVATAAAACXBIWXMAABYlAAAWJQFJUiTwAAABY0lEQVRIx2P4z0AdyEBzg1DAdIYfQJgCZHmCWdsYMAFRBs0BC2UAWT5g1p6hbZAggwIcrgALVQNZSWDWAQY24g3qwRtJ/xgeMqxkCGJgotQgGLzAoEUdg/4zvGQQIxzYLAyODF/gQv0MlgwWDK4MOQxbgV5DKG0nLtZ2wIUykII2EMmoU8QZtAWrQQwMB+HiDygzaDNc/CQlBskwfIKLN5JrkAxDFsMTuOh9BiFSDXoHDI2HDB9RlJ1kECc2r20hkI5OMXhQxyAQzCTNoDJgaAgAvaLLEMkwn+EbkuLvDBLkR78yUoD/Z0gn3yAGhnwk5V2UGBRGLYNmICkvIGzQLqwG8TA0oJQAVvgMymcoYehg+AUXWgoM0kygWC/DbpQ4+89wjYERt0FiRNeNX4GlFJ505EykMacZDPGn7HwCBnxiOMcwjcGJcOEvzqADh2vBQk1AVhaYdZCBc7TKpqJBA9ZiAwDMH49EXcmY2QAAAABJRU5ErkJggg=='
 const VAR_SLUSHPOOL_API_KEY = 'Zm1oK8hGaDExOBaE'; // process.env.VAR_SLUSHPOOL_API_KEY;
 let slushpool_api_token = VAR_SLUSHPOOL_API_KEY;
 
-let BTC_BALANCE = 0.75; // Previous or additional balance to track
+let BTC_BALANCE = 0.78711908; 	// Example amount to track
 let BTC_REWARD_BALANCE = 0;
 let BTC_TO_USD = 0;
-
-let hash_rates = [];
-let workers = [];
 
 const getBTCToUSD = async (amount) => {
 	let result;
@@ -47,11 +22,9 @@ const getBTCToUSD = async (amount) => {
 	}
 
 	req = await https.request(options, res => {
-		res.on('data', d => {
-			// console.log(d.toString());
-			result = d.toString();
-		});
+		res.on('data', d => { result = d.toString(); });
 	});
+
 	req.on('error', error => { console.error(error); });
 	req.end();
 	await once(req, 'close')
@@ -60,7 +33,6 @@ const getBTCToUSD = async (amount) => {
 
 const getSlushpoolProfile = async (apikey) => {
 	let result;
-	let pending_rewards;
 	const headers = { 'User-Agent': 'request', 'SlushPool-Auth-Token': apikey };
 	let options = {
 		hostname: 'slushpool.com',
@@ -72,25 +44,16 @@ const getSlushpoolProfile = async (apikey) => {
 
 	let req = await https.get(options, res => {
 		let json = '';
-		res.on('data', function (chunk) {
-			json += chunk;
-		});
-
+		res.on('data', function (chunk) { json += chunk; });
 		res.on('end', async () => {
 			if (res.statusCode === 200) {
 				try {
 					let data = JSON.parse(json);
 					result = data.btc;
-				} catch (e) {
-					console.log('Error parsing JSON!');
-				}
-			} else {
-				console.log('Status:', res.statusCode);
-			}
+				} catch (e) { console.log('Error parsing JSON!'); }
+			} else { console.log('Status:', res.statusCode); }
 		});
-	}).on('error', function (err) {
-		console.log('Error:', err);
-	});
+	}).on('error', function (err) { console.log('Error:', err); });
 	await once(req, 'close')
 	return result
 };
@@ -108,21 +71,15 @@ const getSlushpoolStats = async (apikey) => {
 
 	let req = await https.get(options, res => {
 		let json = '';
-		res.on('data', function (chunk) {
-			json += chunk;
-		});
+		res.on('data', function (chunk) { json += chunk; });
 		res.on('end', function () {
 			if (res.statusCode === 200) {
 				try {
 					let data = JSON.parse(json);
 					let worker_keys = Array.from(Object.keys(data.btc.workers));
 					result = { worker_keys, ...data.btc, };
-				} catch (e) {
-					console.log('Error parsing JSON!');
-				}
-			} else {
-				console.log('Status:', res.statusCode);
-			}
+				} catch (e) { console.log('Error parsing JSON!'); }
+			} else { console.log('Status:', res.statusCode); }
 		});
 	}).on('error', function (err) {
 		console.log('Error:', err);
@@ -144,24 +101,6 @@ const collectSlushpoolInfo = async () => {
 	}
 }
 
-const debugSlushpoolOutputAndFormat = async (bitbar) => {
-	const { profile, stats, total_btc, total_btc_in_usd } = bitbar;
-	const { estimated_reward, hash_rate_unit, hash_rate_5m, hash_rate_60m, hash_rate_24h, ok_workers } = profile;
-	console.log(`${total_btc} BTC, \$${parseFloat(total_btc_in_usd).toFixed(2)} `);
-	console.log(`${parseFloat(hash_rate_5m).toFixed(2)} (5m) ${hash_rate_unit}, ${parseFloat(hash_rate_60m).toFixed(2)} (60m) ${hash_rate_unit}, ${parseFloat(hash_rate_24h).toFixed(2)} (24 / h) ${hash_rate_unit}, OK WORKERS: ${ok_workers} `);
-	const { worker_keys, workers } = stats;
-	worker_keys.forEach(k => {
-		if (workers[k].state == 'OK') {
-			console.log(k);
-			console.log(workers[k]);
-		}
-	});
-}
-
-const printBitbar = async (bitbar) => {
-	console.log(`⛏️`);
-}
-
 (async () => {
 
 	let boldColor = 'white';
@@ -173,9 +112,14 @@ const printBitbar = async (bitbar) => {
 		boldColor = 'black';
 	}
 
-	// verify key information is available
 	const bitbar = await collectSlushpoolInfo();
-	// debugSlushpoolOutputAndFormat(bitbar);
-	printBitbar(bitbar);
-
+	const { profile, stats, total_btc, total_btc_in_usd } = bitbar;
+	const { estimated_reward, hash_rate_unit, hash_rate_5m, hash_rate_60m, hash_rate_24h, ok_workers } = profile;
+	console.log(`${total_btc} BTC, \$${parseFloat(total_btc_in_usd).toFixed(2)}`);
+	console.log(`${parseFloat(hash_rate_5m).toFixed(2)}(5m) ${hash_rate_unit},${parseFloat(hash_rate_60m).toFixed(2)}(60m) ${hash_rate_unit},${parseFloat(hash_rate_24h).toFixed(2)}(24/h) ${hash_rate_unit}`);
+	console.log(`OK WORKERS: ${ok_workers}`);
+	const { worker_keys, workers } = stats;
+	worker_keys.forEach(k => {
+		if (workers[k].state == 'OK') { console.log(k); console.log(workers[k]); }
+	})
 })();
